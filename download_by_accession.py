f4c0b6c9b2982f736c0e8aa9aed6d6867c6bf37f
Ariana <arianafamiliar@hotmail.com>
2021-10-12 15:59:49 -0400

    Add download_by_accession.py

---
+++ download_by_accession.py
@@ -0,0 +1,37 @@
+# This code uses the Ambra SDK package to access the Ambra API
+#   Looks up a given study based on accession number & then 
+#   uses the study info to download to local as a zip.
+#
+#   amf
+#   Oct 2021
+#
+
+from ambra_sdk.api import Api
+from ambra_sdk.models import Study
+import shutil
+
+url = 'https://access.dicomgrid.com/api/v3/'
+username = '<ambra-user-email>'
+password = '<ambra-user-password>'
+
+api = Api.with_creds(url, username, password)
+
+# find the study
+access_num = '<an-accession-number>'
+study = api \
+    .Study \
+    .list() \
+    .filter_by(Study.accession_number==access_num) \
+    .first()
+
+# download the study
+r = api.Storage.Study.download(engine_fqdn=study.engine_fqdn, \
+                           namespace=study.storage_namespace, \
+                           study_uid=study.study_uid, \
+                           bundle='dicom', \
+                           phi_namespace=study.phi_namespace)
+if r.status_code == 200:
+    with open(access_num+'.zip', 'wb') as f:
+        shutil.copyfileobj(r.raw, f)
+else:
+    print(r.raise_for_status())
